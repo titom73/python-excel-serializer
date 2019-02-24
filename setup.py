118051312efef4a35024142b3067a9fdf783b256
Thomas Grimonet <tgrimonet@arista.com>
2019-02-24 18:11:36 +0100

    Make cleanup for pip installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import sys 
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="inetsix-excel-to-template",
+    version="0.9",
+    scripts=["bin/inetsix-excel-to-template"],
+    python_requires=">=2.7",
+    install_requires=required,
+    url="https://github.com/networkop/docker-topo",
+    license="BSD3",
+    author="Thomas Grimonet",
+    author_email="tom@inetsix.net",
+    description="Jinja2 template rendering from Excel file",
+)
