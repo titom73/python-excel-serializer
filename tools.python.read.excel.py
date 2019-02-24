8e112879478e991cd91bd8298de69ac1d51b4b3a
Thomas Grimonet <tgrimonet@arista.com>
2019-02-24 17:47:43 +0100

    Update code
    Remove functions defined in inetsix module and import this module
    Fix some issues

--- tools.python.read.excel.py
+++ tools.python.read.excel.py
@@ -34,6 +34,7 @@ Text file
 """
 
 import argparse
+import sys
 import os.path
 import yaml
 from jinja2 import Template
@@ -43,67 +44,67 @@ from inetsix.excel import ExcelSerializer
 
 if __name__ == "__main__":
 
-	### CLI Option parser:
-	parser = argparse.ArgumentParser(description="Excel to text file ")
-	parser.add_argument('-e', '--excel', help='Input Excel file',default=None)
-	parser.add_argument('-u', '--url', help='URL for remote Excel file',default=None)
-	parser.add_argument('-s', '--sheet', help='Excel sheet tab',default="Sheet1")
-	parser.add_argument('-m', '--mode', help='Serializer mode: table / list',default="table")
-	parser.add_argument('-n', '--nb_columns', help='Number of columns part of the table',default=6)
-	parser.add_argument('-t', '--template', help='Template file to render',default=None)
-	parser.add_argument('-o', '--output', help='Output file ',default="output.txt")
-	parser.add_argument('-v', '--verbose',action='store_true', help='Increase verbositoy for debug purpose',default=False)
-	options = parser.parse_args()
+    ### CLI Option parser:
+    parser = argparse.ArgumentParser(description="Excel to text file ")
+    parser.add_argument('-e', '--excel', help='Input Excel file',type=str, default=None)
+    parser.add_argument('-u', '--url', help='URL for remote Excel file',type=str, default=None)
+    parser.add_argument('-s', '--sheet', help='Excel sheet tab',type=str, default="Sheet1")
+    parser.add_argument('-m', '--mode', help='Serializer mode: table / list',type=str, default="table")
+    parser.add_argument('-n', '--nb_columns', help='Number of columns part of the table', type=int, default=6)
+    parser.add_argument('-t', '--template', help='Template file to render',default=None)
+    parser.add_argument('-o', '--output', help='Output file ',default="output.txt")
+    parser.add_argument('-v', '--verbose',action='store_true', help='Increase verbositoy for debug purpose', default=False)
+    options = parser.parse_args()
 
-	# Start Engine checklist
-	if options.excel is None and options.url is None:
-		sys.exit("Excel file is missing please provide it with option -e or -u")
-	elif options.excel is not None and options.url is not None:
-		sys.exit("These options are mutually exclusive, please use -e OR -u")
-	elif options.sheet is None:
-		sys.exit("Sheet tab is not define, please provide it with option -s")
-	# elif options.template is None:
-	# 	sys.exit("Jinja 2 Template file is missing please provide it with option -t")
+    # Start Engine checklist
+    if options.excel is None and options.url is None:
+        sys.exit("Excel file is missing please provide it with option -e or -u")
+    elif options.excel is not None and options.url is not None:
+        sys.exit("These options are mutually exclusive, please use -e OR -u")
+    elif options.sheet is None:
+        sys.exit("Sheet tab is not define, please provide it with option -s")
+    # elif options.template is None:
+    #   sys.exit("Jinja 2 Template file is missing please provide it with option -t")
 
-	# Download Excel file from Webserver:
-	if options.url is not None:
-		print " * Downloading Excel file from "+ options.url
-		options.excel = download_file(remote_file= options.url, verbose= options.verbose)
+    # Download Excel file from Webserver:
+    if options.url is not None:
+        print " * Downloading Excel file from "+ options.url
+        options.excel = inetsix.download_file(remote_file= options.url, verbose= options.verbose)
 
-	# Serialize EXCEL file
-	print " * Reading excel file: "+options.excel
-	table = ExcelSerializer(excel_path=options.excel)
-	if options.mode == "table":
-		table.serialize_table( sheet=options.sheet, nb_columns=options.nb_columns)
-	elif options.mode == "list":
-		table.serialize_list( sheet=options.sheet)
-	else:
-		sys.exit("Error: unsuported serializer mode")
+    # Serialize EXCEL file
+    print " * Reading excel file: "+options.excel
+    table = ExcelSerializer(excel_path=options.excel)
+    if options.mode == "table":
+        table.serialize_table( sheet=options.sheet, nb_columns=int(options.nb_columns))
+    elif options.mode == "list":
+        table.serialize_list( sheet=options.sheet)
+    else:
+        sys.exit("Error: unsuported serializer mode")
 
-	if options.verbose is True:
-		print " ** Debug Output **"
-		if table.get_yaml(sheet=options.sheet) is None:
-			print "    -> Sheet has not been found in the structure"
-		else:
-			print yaml.safe_dump(table.get_yaml(sheet=options.sheet), encoding='utf-8', allow_unicode=True)
-		print " ** Debug Output **"
+    if options.verbose is True:
+        print " ** Debug Output **"
+        if table.get_yaml(sheet=options.sheet) is None:
+            print "    -> Sheet has not been found in the structure"
+        else:
+            print yaml.safe_dump(table.get_yaml(sheet=options.sheet), encoding='utf-8', allow_unicode=True)
+        print " ** Debug Output **"
 
-	# Jinja2 template file.
-	# Open JINJA2 file as a template
-	if options.template is not None and options.output is not None:
-		print " * Template rendering"
-		print "    > Use template: "+ options.template
-		print "    > Output file: "+ options.output
+    # Jinja2 template file.
+    # Open JINJA2 file as a template
+    if options.template is not None and options.output is not None:
+        print " * Template rendering"
+        print "    > Use template: "+ options.template
+        print "    > Output file: "+ options.output
 
-		with open( options.template ) as t_fh:
-			t_format = t_fh.read()
-		template = Template( t_format )
-		# Create output file 
-		confFile = open( options.output,'w')
-		confFile.write( template.render( table.get_yaml() ) )
-		confFile.close()
-	else:
-		if table.get_yaml(sheet=options.sheet) is None:
-			print "    -> Sheet has not been found in the structure"
-		else:
-			print yaml.safe_dump(table.get_yaml_all(), encoding='utf-8', allow_unicode=True)
+        with open( options.template ) as t_fh:
+            t_format = t_fh.read()
+        template = Template( t_format )
+        # Create output file 
+        confFile = open( options.output,'w')
+        confFile.write( template.render( table.get_yaml() ) )
+        confFile.close()
+    else:
+        if table.get_yaml(sheet=options.sheet) is None:
+            print "    -> Sheet has not been found in the structure"
+        else:
+            print yaml.safe_dump(table.get_yaml_all(), encoding='utf-8', allow_unicode=True)
