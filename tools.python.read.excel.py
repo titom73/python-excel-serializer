7743c4789501ad70cdf8a90bd81aaeafce38dbb5
Thomas Grimonet <titom73@users.noreply.github.com>
2019-02-24 17:32:36 +0100

    Update script
    Update script to use inetsix lib instead of embedded functions and class

--- tools.python.read.excel.py
+++ tools.python.read.excel.py
@@ -38,35 +38,8 @@ import os.path
 import yaml
 from jinja2 import Template
 import urllib2
-
-from jtools import *
-from jtools.excel import *
-
-
-def download_file( remote_file=None, verbose=False):
-	url = remote_file
-	file_name = url.split('/')[-1]
-	u = urllib2.urlopen(url)
-	f = open(file_name, 'wb')
-	meta = u.info()
-	file_size = int(meta.getheaders("Content-Length")[0])
-	if verbose is True:
-		print " * Downloading: %s Bytes: %s" % (file_name, file_size)
-	file_size_dl = 0
-	block_sz = 8192
-	while True:
-	    buffer = u.read(block_sz)
-	    if not buffer:
-	        break
-	    file_size_dl += len(buffer)
-	    f.write(buffer)
-	    if verbose is True:
-		    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-		    status = status + chr(8)*(len(status)+1)
-		    print "    > "+status,
-	f.close()
-	return file_name
-
+import inetsix
+from inetsix.excel import ExcelSerializer
 
 if __name__ == "__main__":
 
